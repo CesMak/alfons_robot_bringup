b402d45adea423a1063005321a796d2f2f951cbd
lamprecht <lamprecht@sim.tu-darmstadt.de>
2019-03-16 18:21:31 +0100

    add license

--- src/robot_control/robot_control.ino
+++ src/robot_control/robot_control.ino
@@ -2,7 +2,7 @@
    @author  Markus Lamprecht
    @date    March 2019
    @link    www.simact.de/about_me
-
+   @Copyright (c) 2019 Markus Lamprecht. BSD
  *****************************************************************************/
 
 // Problems:
